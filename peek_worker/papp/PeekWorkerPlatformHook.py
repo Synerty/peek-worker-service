2face44af7595a3d83edd5ad79fceb5d16733a48
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-11 15:52:20 +1000

    Debugged depencency issues in PappLoaderABC

--- peek_worker/papp/PeekWorkerPlatformHook.py
+++ peek_worker/papp/PeekWorkerPlatformHook.py
@@ -1,5 +1,11 @@
+from typing import overload
+
 from papp_base.worker.PeekWorkerPlatformHookABC import PeekWorkerPlatformHookABC
 
 
 class PeekWorkerPlatformHook(PeekWorkerPlatformHookABC):
-    pass
+
+    def getOtherPappApi(self, pappName:str):
+        """ Get Other Papp API
+        """
+        raise Exception("Workers don't share APIs")
