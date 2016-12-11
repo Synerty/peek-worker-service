3de9a3d3ca0b9f50cb8c87aa62651e4f16e333b5
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-11 23:57:38 +1000

    Updated to version 0.0.1

--- peek_worker/__init__.py
+++ peek_worker/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'peek'
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 from txhttputil.util.ModuleUtil import filterModules
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 package_name = "peek_worker"
-package_version = '0.0.2'
+package_version = '0.0.1'
 
 egg_info = "%s.egg-info" % package_name
 if os.path.isdir(egg_info):
