c3449638fc8b6fb867100aba8f6405471d8955d6
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-18 23:50:32 +1000

    Debugging package drag and drop update

--- peek_worker/sw_install/PluginSwInstallManager.py
+++ peek_worker/sw_install/PluginSwInstallManager.py
@@ -2,12 +2,12 @@ import logging
 
 from twisted.internet import reactor
 
-from peek_platform.sw_install.PluginSwInstallManagerBase import PluginSwInstallManagerBase
+from peek_platform.sw_install.PluginSwInstallManagerABC import PluginSwInstallManagerABC
 from peek_worker.sw_install.PeekSwInstallManager import peekSwInstallManager
 
 logger = logging.getLogger(__name__)
 
-class PluginSwInstallManager(PluginSwInstallManagerBase):
+class PluginSwInstallManager(PluginSwInstallManagerABC):
     def notifyOfPluginVersionUpdate(self, pluginName, targetVersion):
         logger.info("%s software update to %s complete", pluginName, targetVersion)
         reactor.callLater(1.0, peekSwInstallManager.restartProcess)
