854ea866b3eb305f61adecd2eac4ca5b42a73e21
brentonford <brenton.ford@synerty.com>
2016-12-21 16:22:56 -0500

    import had unresolved variable

--- peek_worker/sw_install/PluginSwInstallManager.py
+++ peek_worker/sw_install/PluginSwInstallManager.py
@@ -3,13 +3,12 @@ import logging
 from twisted.internet import reactor
 
 from peek_platform import PeekPlatformConfig
-from peek_platform.sw_install.PluginSwInstallManagerABC import PluginSwInstallManagerABC
+from peek_platform.sw_install.PluginSwInstallManagerBase import PluginSwInstallManagerBase
 
 logger = logging.getLogger(__name__)
 
-class PluginSwInstallManager(PluginSwInstallManagerABC):
+
+class PluginSwInstallManager(PluginSwInstallManagerBase):
     def notifyOfPluginVersionUpdate(self, pluginName, targetVersion):
         logger.info("%s software update to %s complete", pluginName, targetVersion)
         reactor.callLater(1.0, PeekPlatformConfig.peekSwInstallManager.restartProcess)
-
-
