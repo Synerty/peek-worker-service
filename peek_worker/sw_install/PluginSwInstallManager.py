cd506773eecbc1803f0c4e2c36c116c39f54eba9
brentonford <brenton.ford@synerty.com>
2016-12-22 07:50:26 -0500

    Revert "import had unresolved variable"
    This reverts commit 854ea866b3eb305f61adecd2eac4ca5b42a73e21.

--- peek_worker/sw_install/PluginSwInstallManager.py
+++ peek_worker/sw_install/PluginSwInstallManager.py
@@ -3,12 +3,13 @@ import logging
 from twisted.internet import reactor
 
 from peek_platform import PeekPlatformConfig
-from peek_platform.sw_install.PluginSwInstallManagerBase import PluginSwInstallManagerBase
+from peek_platform.sw_install.PluginSwInstallManagerABC import PluginSwInstallManagerABC
 
 logger = logging.getLogger(__name__)
 
-
-class PluginSwInstallManager(PluginSwInstallManagerBase):
+class PluginSwInstallManager(PluginSwInstallManagerABC):
     def notifyOfPluginVersionUpdate(self, pluginName, targetVersion):
         logger.info("%s software update to %s complete", pluginName, targetVersion)
         reactor.callLater(1.0, PeekPlatformConfig.peekSwInstallManager.restartProcess)
+
+
