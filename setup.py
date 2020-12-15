78cf1b3ba2b02fa1f17c0ae3b23c0fb0ec4045d0
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-12-15 20:39:02 +1000

    PEEK-591: Added peek pkg version dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 import os
 import shutil
-from setuptools import setup
+from setuptools import setup, find_packages
 
-pip_package_name = "peek-worker-service"
+###############################################################################
+# Define variables
+#
+# Modify these values to fork a new plugin
+#
+
+author = "Synerty"
+author_email = 'contact@synerty.com'
 py_package_name = "peek_worker_service"
+pip_package_name = py_package_name.replace('_', '-')
 package_version = '0.0.0'
+description = 'Peek Worker Service.'
+
+download_url = 'https://bitbucket.org/synerty/%s/get/%s.zip'
+download_url %= pip_package_name, package_version
+url = 'https://bitbucket.org/synerty/%s' % pip_package_name
+
+###############################################################################
+# Customise the package file finder code
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
@@ -37,26 +53,45 @@ def find_package_files():
 
 package_files = find_package_files()
 
+###############################################################################
+# Define the dependencies
+
+# Ensure the dependency is the same major number
+# and no older then this version
+
+requirements = [
+    "peek-platform"
+]
+
+# Force the dependencies to be the same branch
+reqVer = '.'.join(package_version.split('.')[0:2]) + ".*"
+
+# >=2.0.*,>=2.0.6
+requirements = ["%s==%s,>=%s" % (pkg, reqVer, package_version)
+                if pkg.startswith("peek") else pkg
+                for pkg in requirements]
+
+###############################################################################
+# Call the setuptools
+
 setup(
-    name=pip_package_name,
-    packages=[py_package_name],
-    package_data={'': package_files},
     entry_points={
         'console_scripts': [
             'run_peek_worker_service = peek_worker_service.run_peek_worker_service:main',
             'winsvc_peek_worker_service = peek_worker_service.winsvc_peek_worker_service:main',
         ],
     },
-    install_requires=["peek-platform"],
-    zip_safe=False,version=package_version,
-    description='Peek Platform - Worker Service',
-    author='Synerty',
-    author_email='contact@synerty.com',
-    url='https://github.com/Synerty/%s' % pip_package_name,
-    download_url='https://github.com/Synerty/%s/tarball/%s' % (
-        pip_package_name, package_version),
+    name=pip_package_name,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data={'': package_files},
+    install_requires=requirements,
+    zip_safe=False,
+    version=package_version,
+    description=description,
+    author=author,
+    author_email=author_email,
+    url=url,
+    download_url=download_url,
     keywords=['Peek', 'Python', 'Platform', 'synerty'],
-    classifiers=[
-        "Programming Language :: Python :: 3.5",
-    ],
+    classifiers=[],
 )
